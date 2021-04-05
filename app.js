9addfc06f583486740778808ed93282cdad06392
doramakh <anvarovna28@gmail.com>
2021-04-05 18:54:42 +0500

    run app for the first time

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.get("/", (_, res) => {
+    res.send("OK")
+}) 
+
 app.listen(3000, console.log('App is running on localhost: 3000'))
