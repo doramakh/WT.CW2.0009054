9a07080b4d201a5c8c07cb769eea075130609c3a
doramakh <anvarovna28@gmail.com>
2021-04-06 16:40:33 +0500

    create hero page html

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static("public"))
+app.set("view engine", "ejs")
+
+const PORT = process.env.PORT || 3000
+
 app.get("/", (_, res) => {
-    res.send("OK")
+    res.render("index")
 }) 
 
-app.listen(3000, console.log('App is running on localhost: 3000'))
+app.listen(PORT, () => {
+    console.log("Server ready at local host:" + PORT)
+})
