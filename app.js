918acfeb8b3fd59024813dafa02bca5fde0f2bef
doramakh <anvarovna28@gmail.com>
2021-04-06 17:55:56 +0500

    add route

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require('express')
 const app = express()
 
+const PORT = process.env.PORT || 3000
+
+const fileUpload = require("express-fileupload")
+// const ejs = require("ejs")
+
+const Home = require("./routes/home")
+const Posts = require("./routes/posts")
+const Post = require("./routes/post")
+const CreatePost = require("./routes/create-post")
+
+app.use(express.json())
+app.use(fileUpload())
+app.use(express.urlencoded({ extended: true }))
+
 app.use(express.static("public"))
 app.set("view engine", "ejs")
 
-const PORT = process.env.PORT || 3000
+app.get("/", Home.GET)
+app.get("/post/:id", Post.GET)
+app.get("/posts", Post.GET)
+app.get("/create-post", CreatePost.GET)
+app.post("/create-post", CreatePost.POST)
 
 app.get("/", (_, res) => {
     res.render("index")
@@ -13,3 +31,6 @@ app.get("/", (_, res) => {
 app.listen(PORT, () => {
     console.log("Server ready at local host:" + PORT)
 })
+
+
+app.listen(PORT, console.log(PORT))
